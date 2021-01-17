b9f6eff321a6411e645ecf35f2b610967d4687c8
clvrk <52180876+clvrk@users.noreply.github.com>
2021-01-17 10:00:23 -0800

    Done

--- bot.py
+++ bot.py
@@ -13,14 +13,20 @@ intents = discord.Intents.all()
 client = commands.Bot(command_prefix='!', help_command=None, intents=intents)
 
 
+@client.event()
+async def on_ready():
+	await client.wait_until_ready()
+	print('online')
+	
 @client.event
 async def on_member_join(member):
 	await sleep(60*10)
-	for channel in member.guild.channels:
+	for channel in member.guild.channels: ## looping through guild channels and then checking for the one that starts with "member" so it can be changed into a member count channel.
 		if channel.name.startswith('Member'):
 			await channel.edit(name=f'Members: {member.guild.member_count}')
 			break
-
+	
+## looping through guild channels and then checking for the one that starts with "member" so it can be changed into a member count channel.
 
 
 
