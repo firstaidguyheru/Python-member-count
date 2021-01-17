54626d2df614d225888860952f696c943e80afa5
clvrk <jheru711@Gmail.com>
2021-01-17 12:29:53 -0400

    Finishing touches

---
+++ README.md
@@ -0,0 +1,1 @@
+# Git clone and add an env file where you should put your bot's token and run the code.
--- bot.py
+++ bot.py
@@ -1,6 +1,13 @@
 import discord
 from discord.ext import commands
 from asyncio import sleep
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
 
 intents = discord.Intents.all()
 client = commands.Bot(command_prefix='!', help_command=None, intents=intents)
@@ -8,8 +15,15 @@ client = commands.Bot(command_prefix='!', help_command=None, intents=intents)
 
 @client.event
 async def on_member_join(member):
-	await sleep(10)
+	await sleep(60*10)
 	for channel in member.guild.channels:
 		if channel.name.startswith('Member'):
 			await channel.edit(name=f'Members: {member.guild.member_count}')
 			break
+
+
+
+
+
+
+client.run(os.getenv("TOKEN"))
