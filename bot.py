46109e3a4a754296a1aa883854b9cdd19cd596e4
clvrk <52180876+clvrk@users.noreply.github.com>
2021-01-28 05:55:39 -0800

    man

--- bot.py
+++ bot.py
@@ -21,15 +21,12 @@ async def on_ready():
 @client.event
 async def on_member_join(member):
 	await sleep(60*10)
-	for channel in member.guild.channels: ## looping through guild channels and then checking for the one that starts with "member" so it can be changed into a member count channel.
+	for channel in member.guild.channels: 
 		if channel.name.startswith('Member'):
 			await channel.edit(name=f'Members: {member.guild.member_count}')
 			break
 	
 ## looping through guild channels and then checking for the one that starts with "member" so it can be changed into a member count channel.
-
-
-
 
 
 client.run(os.getenv("TOKEN"))
